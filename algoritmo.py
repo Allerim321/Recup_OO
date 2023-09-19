6bdadb94af94b0e4bfb3aa73c80240c434c51060
Mirella <mirellatcg@gmail.com>
2023-09-19 08:23:59 -0300

    up

--- algoritmo.py
+++ algoritmo.py
@@ -12,7 +12,7 @@ while on==1:
             resp=int(input("->"))
             match resp:
                 case 1:
-                os.system('cls')
+                    os.system('cls')
                     print("Criar conta")
                     nome=input("Diga seu nome:\n->")
                     saldo_inicial=int(input("Diga seu saldo inicial:\n->"))
@@ -23,20 +23,28 @@ while on==1:
                 case 2:
                     os.system('cls')
                     print("Sacar")
-
+                    conta=input("Digite o nome da sua conta.\n->")
+                    valor=int(input("Digite o valor que queira retirar.\n->"))
+                    banco.sacar(conta, valor)
+                    
                 case 3:
                     os.system('cls')
+                    print("Depositar")
+                    conta=input("")
+                    valor=int(input(""))
 
                 case 4:
                     os.system('cls')
-
+                    print("Transferir")
+                    conta=input("")
+                    valor=int(input(""))
                 case 5:
                     os.system('cls')
 
                 case 6:
                     on=0
                     os.system('cls')
-                case_:
+                case _:
                     print("Opção inválida.")
                     os.system('pause')
 
--- classes.py
+++ classes.py
@@ -2,21 +2,25 @@ class Banco:
 
     contas=[]
     conta_individual=[]
-    def criar_conta(self, nome, saldo_inicial):
+    def criar_conta(self, nome, saldo_inicial, saldo):
         self.nome=nome
         self.saldo_inicial=saldo_inicial
+        sale.saldo=None
         conta_individual.append(nome, saldo_inicial)
         contas.append(conta_individual())
 
-    def sacar(self, conta, valor):
+    def sacar(self, conta, valor, saldo):
+        self.saldo=criar_conta(saldo_inicial, saldo)
         self.conta=conta
         self.valor=valor
-
+        saldo=saldo
+        
     def depositar(self, conta, valor):
-        pass
+        self.conta=conta
+        self.valor=valor
 
     def transferir(self, origem, destino, valor):
         pass
 
     def saldo(conta):
-        pass
+        print (conta_individual())
